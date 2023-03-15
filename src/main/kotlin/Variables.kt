f31e15b3871bba264be3a90f5871ab2037322b3e
Gichezz <rgich123@gmail.com>
2023-03-15 10:32:38 +0300

    first commit

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,34 @@
+fun main(args: Array<String>) {
+    var x:Int
+    var y:Float
+    var w:Double
+    var m:Char
+    var v:String
+    var j:Boolean
+    var answer:Double
+    var age:Int
+    var name:String
+
+    x = 10
+    y = 20.0f
+    w = 30.0
+    m = 'a'
+    v = "Ryan"
+    j = true
+
+    answer = y + w
+    println(answer)
+
+    //Concatenation
+    age = 100
+    name = "King"
+    println("Hello "+name+ ", you are "+age+ " years old")
+    println("Hello $name, you are $age years old")
+
+    var firstnum = 20
+    var secondnum = 30
+    var jibu = firstnum + secondnum
+
+    println("Your answer is $jibu")
+
+}
